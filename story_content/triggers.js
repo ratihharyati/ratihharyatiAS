2a623ae118439120e56926d145ca5aac18353197
ratihharyati <ratih.27haryati@gmail.com>
2024-11-11 15:24:20 +0700

    Git Upload

---
+++ story_content/triggers.js
@@ -0,0 +1,24 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Q9r9TIOWhi":
+        Script1();
+        break;
+      case "66HdApBclUT":
+        Script2();
+        break;
+      case "5apVJg3RhSp":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,42 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var currentDate = new Date()
+var day = currentDate.getDate()
+var month = currentDate.getMonth() + 1
+var year = currentDate.getFullYear();
+var player = GetPlayer();
+var newName = day + "/" + month + "/" +year
+player.SetVar("DateValue", newName);
+}
+
+window.Script2 = function()
+{
+  window.print();
+}
+
+window.Script3 = function()
+{
+  Java Script untuk Time di sertifikat:
+
+var currentDate = new Date()
+var day = currentDate.getDate()
+var month = currentDate.getMonth() + 1
+var year = currentDate.getFullYear();
+var player = GetPlayer();
+var newName = day + "/" + month + "/" +year
+player.SetVar("DateValue", newName);
+
+
+Java Script Untuk di Tombol Print:
+
+window.print(); 
+}
+
+};
